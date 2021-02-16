4b3bc75a4dfc2840f28f5b6b6a7974d2c1264f9f
Diego Herrera <jdht1992@gmail.com>
2021-02-16 12:03:02 -0600

    new congfiguration in bd

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ from flask_marshmallow import Marshmallow
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'this-really-needs-to-be-changed'
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql+psycopg2://jdht:@localhost/cargamos10'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql+psycopg2://jdht:@localhost/cargamos'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 # initialize the database connection
